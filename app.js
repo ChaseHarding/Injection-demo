3facdc4688ec5b3f3c2616f7845107fa64c72da1
ChaseHarding <chaseharding965@gmail.com>
2024-05-01 20:05:55 -0700

    mock database that will hold our login information

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ express = require('express'),
 bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
+
+const db = new sqlite3.Database(':memory:');
+db.serialize(function () {
+ db.run(“CREATE TABLE user (username TEXT, password TEXT, title TEXT)“);
+ db.run(“INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')“);
+});
+
+//MIDDLEWARE
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+
